1c30c41bf3088b2fed7754b8d516a223d624f7cd
Nazar Vaskiv <vaskiv27@gmail.com>
2025-09-17 13:33:34 +0300

    - fix config

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,19 @@
-import typescript from "@rollup/plugin-typescript";
+const typescript = require('@rollup/plugin-typescript')
 
-export default {
-  input: "./src/index.ts",
+module.exports = {
+  input: './src/index.ts',
   output: [
     {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-      exports: "default",
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      exports: 'auto'
     },
     {
-      file: "dist/index.esm.js",
-      format: "es",
-    },
+      file: 'dist/index.esm.js',
+      format: 'es',
+      exports: 'auto'
+    }
   ],
   external: [],
-  plugins: [typescript({ tsconfig: "./tsconfig.json", sourceMap: false, declaration: true, outDir: "dist", exclude: ["**/*.test.ts"] })],
-};
+  plugins: [typescript({ tsconfig: './tsconfig.json', sourceMap: false, declaration: true, outDir: 'dist', exclude: ['**/*.test.ts'] })]
+}
